7a4f298c3a64216149965adeaccad1cf2d55d599
cawleygl <grantlcawley@gmail.com>
2021-02-08 06:55:40 -0600

    Added database schema

---
+++ schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_coordinatorDB;
+
+CREATE database employee_coordinatorDB;
+
+USE employee_coordinatorDB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL, 
+  FOREIGN KEY (role_id) REFERENCES role (id),
+  manager_id INT NULL, 
+  FOREIGN KEY (manager_id) REFERENCES employee (id)
+);
